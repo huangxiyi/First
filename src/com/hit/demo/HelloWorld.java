4e064e39c15f48e6d5c1796d689193325bc09ae1
huangxiyi <huangxiyi@shouqianba.cn>
2017-11-14 18:00:18 +0800

    Initial commit

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.hit.demo;
+
+/**
+ * Created by Administrator on 2017/11/14/014.
+ */
+public class HelloWorld {
+   public static void main(String[]args){
+       String a="abc";
+       System.out.println("Hello World");
+       System.out.println(111);
+       System.out.println(22);
+       System.out.println(a);
+
+    }
+
+}
